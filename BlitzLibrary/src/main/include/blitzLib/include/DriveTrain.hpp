3682fe6dca83882d27d92c6a6e37698f571cac72
Theo Buchholz <theobuchholz@gmail.com>
2019-02-28 08:26:04 -0600

    added DriveTrain class as an abstract class

---
+++ BlitzLibrary/src/main/include/blitzLib/include/DriveTrain.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <frc/WPILib.h>
+#include <ctre/Phoenix.h>
+
+#include "DataTypes.hpp"
+#include "DriveReferences.hpp"
+#include "BlitzLogger.hpp"
+
+namespace Blitz
+{
+    class DriveTrain
+    {
+        public:
+            DriveTrain(Blitz::BlitzLogger *Logger);
+
+            void SetMotorDirection(int Motor, int dir);
+            double GetMotorOutput(int MotorID);
+
+            void EnablePID(bool enabled);
+            void TuneF(int MotorID, double FGain);
+            void TuneP(int MotorID, double PGain);
+            void TuneI(int MotorID, double IGain);
+            void TuneD(int MotorID, double DGain);
+
+            virtual void Initialize();
+            virtual void Drive();
+            virtual void Close();
+
+        protected:
+            Blitz::BlitzLogger *Logger;
+            Blitz::Models::DriveMotors Motors;
+            
+            int MotorDirs[4] = {1, 1, 1, 1};
+            double motorValues[4] = {0, 0, 0, 0};
+            int NumOfMotors = 4;
+            bool usePID = false;
+
+        private:
+            TalonSRX LeftFrontMotor, LeftBackMotor, RightFrontMotor, RightBackMotor;
+
+    };
+}
